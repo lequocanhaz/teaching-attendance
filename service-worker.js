eddab3debf244135763e00ebf770a04f00993b3c
lequocanhaz <lequocanhaz@gmail.com>
2026-09-08 00:55:38 +0700

    Bump service worker cache to V4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="teaching-attendance-v3";
+const CACHE="teaching-attendance-v4";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
