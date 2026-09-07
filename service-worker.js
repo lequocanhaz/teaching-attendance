61085fc031fe437c742ce45313882b81d1c55c6c
lequocanhaz <lequocanhaz@gmail.com>
2026-09-08 00:37:09 +0700

    V3: refresh cached assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,8 @@
-const CACHE="teaching-attendance-v2";
+const CACHE="teaching-attendance-v3";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));
+});
